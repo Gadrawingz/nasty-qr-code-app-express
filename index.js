0c5d9b63a203552533a75a7a4982812abf8ffd6d
Gad Iradufasha <legmeekmc4@gmail.com>
2024-10-02 23:09:26 +0200

    Added main codes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const QRcode = require('qrcode')
+const QRCode = require('QRCode')
 const app = express();
 
 const PORT = 3003;
@@ -7,8 +7,34 @@ const PORT = 3003;
 app.get('/qr/generate', async(req, res) => {
     try {
         const url = req.query.url || 'https://donnekt.com';
-        const qrCodeImage = await QRcode.toDataURL(url);
-        res.send(`<img src="${qrCodeImage}" alt="QR CODE STUFF"/>`)
+        const QRCodeImage = await QRCode.toDataURL(url);
+        res.send(`<img src="${QRCodeImage}" alt="QR CODE STUFF"/>`);
+
+        /*******************************************/
+        
+        /*******************************************/
+        // 01. Lemme create data that I want to hhide in QR Code:
+        let userData = {
+            username: "gadrone",
+            identity: 43202233398,
+            department: "Software Development"
+        };
+
+        // 02. Converting data in order to simplify furher operations
+        // Converting the data into String format
+        let convertedData = JSON.stringify(userData);
+
+
+        // 03. Converting the data into base64;
+        QRCode.toDataURL(convertedData, (error, code)=> {
+            if(error) {
+                return console.log("Error please!")
+            } else {
+                console.log(code)
+            }
+        })
+
+
     } catch(err) {
         console.error('Error generating QR code:', err);
         res.status(500).send('Internal Server Error');
